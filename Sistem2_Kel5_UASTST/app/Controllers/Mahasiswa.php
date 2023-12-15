448e2798e727cc42abc875c2fd4a501686cbab77
Ivan Aldy Ganesen <18221045@std.stei.itb.ac.id>
2023-12-16 01:53:05 +0700

    fix: Mahasiswa
    now integrates with database and no hardcoded data

--- Sistem2_Kel5_UASTST/app/Controllers/Mahasiswa.php
+++ Sistem2_Kel5_UASTST/app/Controllers/Mahasiswa.php
@@ -44,28 +44,24 @@ class Mahasiswa extends Controller {
         $nim = $this->request->getPost('nim');
         $password = $this->request->getPost('password');
 
-        // Print nim and password in console
-        echo "NIM: " . $nim . "\n";
-        echo "Password: " . $password . "\n";
-        
-
         // UNBLOCK WHEN DATABASE DONE
-        // $user = $this->modelMahasiswa->getNilaiByNIM($nim);
+        $user = $this->modelMahasiswa->getDataByNIM($nim);
 
-        // For testing purposes, using hardcoded credentials
-        $validNIM = '18221000';
-        $validPassword = '$2a$10$.8ttdgn4FLkTAH6FJTScFuzZMDJ1NNizMpxhHeAn/gb5MDmIW68Ri';
-
-        // UNBLOCK WHEN DATABASE DONE
-        // if ($user && password_verify($password, $user['password'])) {
+        // // For testing purposes, using hardcoded credentials
+        // $validNIM = '18221000';
+        // $validPassword = '$2a$10$.8ttdgn4FLkTAH6FJTScFuzZMDJ1NNizMpxhHeAn/gb5MDmIW68Ri';
 
         $session = session();
-        if ($nim == $validNIM && password_verify($password, $validPassword)) {
+        // UNBLOCK WHEN DATABASE DONE
+        if ($user && password_verify($password, $user['password'])) {
+        // if ($nim == $validNIM && password_verify($password, $validPassword)) {
             // Jika login sukses, set sesi dan redirect ke halaman home
             // Simpan data pengguna ke dalam sesi
             $userData = [
-                'nim' => $nim,
-                'nama' => 'Mahasiswa',
+                'nim' => $user['nim'],
+                'nama' => $user['nama'],
+                'ipk' => $user['ipk'],
+                'totalsks' => $user['totalsks'],
                 // Data pengguna lainnya...
             ];
 
@@ -75,10 +71,10 @@ class Mahasiswa extends Controller {
 
             // Redirect ke halaman home
             return redirect()->to('mahasiswa/home');
-        } else if ($nim == null || $nim != $validNIM) {
+        } else if ($nim == null || $user == null) {
             $session->set('error', 'Wrong NIM!');
             return redirect()->to('mahasiswa/login');
-        } else if ($password == null || !password_verify($password, $validPassword)) {
+        } else if ($password == null || ($user && !password_verify($password, $user['password']))) {
             $session->set('error', 'Wrong Password!');
             return redirect()->to('mahasiswa/login');
         }
