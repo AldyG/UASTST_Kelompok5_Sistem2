3d8e3bd8d5f392e13a476088fa535b0807628657
Ivan Aldy Ganesen <18221045@std.stei.itb.ac.id>
2023-12-16 01:52:37 +0700

    feat: add function getMataKuliahByNIM()
    added new function to fetch mata kuliah from local database

--- Sistem2_Kel5_UASTST/app/Models/ModelMahasiswa.php
+++ Sistem2_Kel5_UASTST/app/Models/ModelMahasiswa.php
@@ -7,12 +7,23 @@ use CodeIgniter\Model;
 class ModelMahasiswa extends Model {
     protected $table = 'mahasiswa'; // Change to the actual table name
 
-    public function getNilaiByNIM($nim) {
+    public function getDataByNIM($nim) {
         // Query the 'mahasiswa' table to get data based on NIM
         $query = $this->where('nim', $nim)->first();
 
         // If a matching user is found, return the data
         // Otherwise, you might want to handle this case based on your requirements
         return ($query) ? $query : null;
+    }
+
+    public function getMataKuliahByNIM($nim) {
+        $sql = "SELECT rencana_studi.kode_matkul, mata_kuliah.nama, mata_kuliah.sks, mahasiswa.ipk
+                FROM rencana_studi
+                JOIN mata_kuliah ON rencana_studi.kode_matkul = mata_kuliah.kode
+                JOIN mahasiswa ON rencana_studi.nim_mahasiswa = mahasiswa.nim
+                WHERE rencana_studi.nim_mahasiswa = $nim";
+
+        // Execute the query and return the result
+        return $this->query($sql, [$nim])->getResultArray();
     }
 }
