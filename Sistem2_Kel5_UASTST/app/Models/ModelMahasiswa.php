6ddb6324f7db797e32cb17e25893ebe796144127
Ivan Aldy Ganesen <18221045@std.stei.itb.ac.id>
2023-12-17 11:01:14 +0700

    fix: getDataByNIM
    re-inserts the getDataByNIM() function which was accidentally deleted.

--- Sistem2_Kel5_UASTST/app/Models/ModelMahasiswa.php
+++ Sistem2_Kel5_UASTST/app/Models/ModelMahasiswa.php
@@ -7,6 +7,15 @@ use CodeIgniter\Model;
 class ModelMahasiswa extends Model {
     protected $table = 'mahasiswa'; // Change to the actual table name
 
+    public function getDataByNIM($nim) {
+        // Query the 'mahasiswa' table to get data based on NIM
+        $query = $this->where('nim', $nim)->first();
+
+        // If a matching user is found, return the data
+        // Otherwise, you might want to handle this case based on your requirements
+        return ($query) ? $query : null;
+    }
+
     public function getMahasiswaByNIM($nim) {
         $sql = "SELECT ipk, totalsks FROM mahasiswa WHERE nim = $nim";
 
